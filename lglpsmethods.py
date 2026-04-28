2b59f5de800bdf511afa7347d650ca4bdd11ac24
sandeep <rskravi15@gmail.com>
2026-04-28 21:53:14 +0530

    pytest added. dependency resolution via poetry

--- lglpsmethods.py
+++ lglpsmethods.py
@@ -110,30 +110,3 @@ class LGL:
         self.D = D
 
 
-if __name__ == "__main__":
-    # plotting results
-    import matplotlib.pyplot as plt
-    # initial and final point
-    x0 = 0
-    xf = 2*np.pi
-    # grid size
-    N = 50
-    # LGL object which has the diff. matrix, quadrature weights and LGL collocation points
-    lgl = LGL(N+1)
-    # domain transformation [-1,1]->[x0,xf]
-    x = lgl.tau*(xf-x0)/2+(xf+x0)/2
-    # sample the function
-    y = np.sin(x)
-    # compute derivative from differentiation matrix
-    y_dot = 2/(xf-x0)*lgl.D@y.reshape(N+1, 1)
-    # plot function and its numerical and analytical derivative
-    plt.plot(x, y, label='sine')
-    plt.plot(x, y_dot, '-o', label='cosine (lgl)')
-    plt.plot(x, np.cos(x), label='cosine (analytical)')
-    # plt.plot(x,y_dot-np.cos(x))
-    plt.legend()
-    plt.grid()
-    plt.show()
-    print('quadrature:', lgl.wi.T@y)
-else:
-    pass
---
+++ test.py
@@ -0,0 +1,31 @@
+import numpy.testing as npt
+import numpy as np
+import pytest
+
+from lglpsmethods import LGL
+
+x0 = 0
+xf = 2 * np.pi
+# grid size
+N = 50
+# LGL object which has the diff. matrix, quadrature weights and LGL collocation points
+lgl = LGL(N + 1)
+# domain transformation [-1,1]->[x0,xf]
+x = lgl.tau * (xf - x0) / 2 + (xf + x0) / 2
+# sample the function
+y = np.sin(x)
+# compute derivative from differentiation matrix
+y_dot = 2 / (xf - x0) * lgl.D @ y.reshape(N + 1, 1)
+y_dot_an=np.cos(x)
+ones=np.ones(N+1)
+zeros=np.zeros(N+1)
+
+
+# integral sinx 0-2pi -> zero
+def integral_sin() -> np.ndarray:
+    return (x[-1] - x[0]) / 2 * np.dot(lgl.wi.ravel(), y)
+
+npt.assert_array_almost_equal(integral_sin(),0,decimal=4)
+npt.assert_array_almost_equal(y_dot,y_dot_an,decimal=4)
+npt.assert_array_almost_equal(lgl.D@ones.reshape(N+1,1),zeros.reshape(N+1,1),decimal=4)
+
